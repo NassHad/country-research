20186c58f5f8cd480626eb5b6ad629a133feed86
Nassim <nassim.haddad.pro@gmail.com>
2021-09-01 13:18:17 +0200

    Project init + navigation + styles

---
+++ client/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>À propos</h1>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus aspernatur vitae voluptatum voluptatem tenetur doloribus corrupti quam ex harum provident earum dicta fuga deleniti, expedita magnam sit dolorem, illo, culpa minima cum. Aliquam iusto, facilis culpa corrupti magni consequuntur ad molestiae odit aliquid qui ullam praesentium porro delectus optio dolorem cupiditate officiis maxime? Enim, animi. Unde laborum quam at porro.
+      </p>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quos corporis illum ipsum. Quod tempora unde et. Repellat aspernatur molestias ad in reprehenderit amet sunt inventore aut ipsam voluptates? Eum laudantium sed architecto numquam reiciendis omnis dolorum amet eveniet aliquam quasi? Eveniet aut eius necessitatibus aliquam! Qui neque laborum cum! Laudantium quasi doloribus iure sit nam minus eum harum quibusdam? Animi quasi magni voluptatibus recusandae itaque tempora aliquid, nihil accusamus neque, illo velit voluptates corrupti totam porro earum! Tempore quaerat dolor non. Rerum a laboriosam assumenda adipisci eligendi? Quod, fugit unde sapiente dolorum saepe blanditiis error nostrum quidem molestiae autem iste fugiat iusto pariatur perferendis repudiandae et suscipit distinctio harum voluptate esse a? Doloremque, suscipit autem. Ipsum commodi asperiores eligendi saepe libero tempora cum quam fugit omnis architecto laboriosam expedita autem, amet deserunt voluptates ipsa voluptas quisquam. Asperiores, eveniet maiores?</p>
+    </div>
+  );
+};
+
+export default About;
